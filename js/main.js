df7321d512df42fa2f7f42325b47fd359ba61d7a
Cheong Kah Hong <kahhong_cheong@git.savantdegrees.com>
2025-01-23 00:30:30 +0800

    Add the rest of the pages

--- js/main.js
+++ js/main.js
@@ -12,4 +12,28 @@ $(document).ready(function(){
     $(".hamburger").on("click",function(){
         $("#navigation").toggleClass("open");
     })
+
+    const mainNavigation = 
+    `<ul>
+        <li>
+            <a href="areas-of-practice.html">Areas of practice</a>
+        </li>
+        <li>
+            <a href="#">IP showcase</a>
+        </li>
+        <li>
+            <a href="#">Accolades</a>
+        </li>
+        <li>
+            <a href="#">Publications</a>
+        </li>
+        <li>
+            <a href="#">About Us</a>
+        </li>
+        <li>
+            <a href="#">Contact</a>
+        </li>
+    </ul>`
+
+    $("#navigation").append(mainNavigation);
 });
